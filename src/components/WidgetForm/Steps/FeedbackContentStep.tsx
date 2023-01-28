81d7fbd158dccc3f42f380a4f5701a6daf264f54
real-tea <akash.0711a@gmail.com>
2023-01-28 10:38:39 +0530

    removing postmark api for sending emails v1.0

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -69,7 +69,7 @@ export function FeedbackContentStep({
       <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
         <textarea
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-          placeholder="Conte com detalhes o que está acontecendo..."
+          placeholder="Your words go here..."
           onChange={event => setComment(event.target.value)}
         />
 
@@ -81,7 +81,7 @@ export function FeedbackContentStep({
             disabled={comment.length === 0 || isSendingFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            {isSendingFeedback ? <Loading /> : 'Enviar feedback'}
+            {isSendingFeedback ? <Loading /> : 'send feedback'}
           </button>
         </footer>
       </form>
